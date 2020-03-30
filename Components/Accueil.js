4f9de936fc8a6a136ba520f53330e2f86519cd1c
ridley <marinette.ridley@gmail.com>
2020-03-29 21:05:47 -0400

    -Changement du nom de Projet -Projet sou Expo -Création de L'accueil (OK) -Icon et Splash screen (splash à changer)

---
+++ Components/Accueil.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import  {StyleSheet,View,Button,Image,Text,Alert, TouchableOpacity,ImageBackground} from 'react-native'
+
+
+
+class Accueil extends React.Component
+{
+  constructor(props) {
+    super(props);
+    this.userName= "Le Rid";
+  }
+
+  render()
+  {
+    return(
+     <View style={styles.main_container}>
+          <View style={styles.logo_container}>
+          <Image style={{height:100, width:100}} source={require('../assets/icon.png')}/>
+          </View>
+
+          <View style={styles.title_container}>
+              <Text style={{
+                  fontSize: 55,
+                  fontWeight:"bold",
+                  color:'#C0C0C0C0'}}>SLEY</Text>
+              <Text style={{
+                  fontSize: 35,
+                  fontWeight:"bold",
+                  color:'yellow'}}>Sport Training</Text>
+          </View>
+          <View style={styles.options_container}>
+              <TouchableOpacity style={styles.touchOption}>
+                <View style={styles.touch_container}>
+                  <Image style={{height:40, width:40}} source={require('../assets/profil.png')}/>
+                  <Text style={styles.text_Common}> Profil</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.touchOption}>
+                <View style={styles.touch_container}>
+                  <Image style={{height:40, width:40}} source={require('../assets/profil.png')}/>
+                  <Text style={styles.text_Common}> Planning</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.touchOption}>
+                <View style={styles.touch_container}>
+                  <Image style={{height:40, width:40}} source={require('../assets/profil.png')}/>
+                  <Text style={styles.text_Common}> Infos pratiques</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.touchOption}>
+                <View style={styles.touch_container}>
+                  <Image style={{height:40, width:40}} source={require('../assets/profil.png')}/>
+                  <Text style={styles.text_Common}> Partenaires</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.touchOption}>
+                <View style={styles.touch_container}>
+                  <Image style={{height:40, width:40}} source={require('../assets/profil.png')}/>
+                  <Text style={styles.text_Common}>À propos </Text>
+                </View>
+              </TouchableOpacity>
+
+          </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  //Containers
+  main_container: {
+    flex: 1,
+    backgroundColor:'black'
+  },
+  logo_container: {
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  title_container: {
+    flex: 1,
+    alignItems:'center'
+  },
+  options_container: {
+    flex: 3
+  },
+  touch_container:{
+    flexDirection:"row"
+  },
+
+
+  //Text
+  text_Common:{
+      fontSize: 25,
+      fontWeight:"bold",
+      color:'#C0C0C0C0'
+  },
+
+  //TouchableOpacity
+  touchOption:{
+      marginBottom:10,
+      /*borderColor:'#A9A9A9',
+      borderWidth:3,
+      borderRadius:35,*/
+      padding:10,
+      left:50,
+      alignSelf:'baseline' // Le parent s'ajuste à l'enfant
+  }
+})
+export default Accueil
