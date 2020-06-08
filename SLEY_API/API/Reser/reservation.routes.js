95fabc6cafff8ba768791bf13c13ef2d5d3b873d
ridley <marinette.ridley@gmail.com>
2020-06-08 11:13:31 -0400

    -Separattion des projets API et APP

--- SLEY_API/API/Reser/reservation.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app =>{
-  const reservations = require("./reservation.controller.js")
-
-  app.post("/reservations", reservations.create )
-  app.get("/reservations", reservations.findAll)
-  app.get("/reservations/:reservationID", reservations.findOne)
-  app.put("/reservations/:reservationID", reservations.update)
-}
